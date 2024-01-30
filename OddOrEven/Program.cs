f147ea9f3581a1da32608b3a4a1337d220582956
k <katrin.vohta@tktk.ee>
2024-01-30 19:34:10 +0300

    week3

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris või paaritu
+
+int result = userNum % 2;
+
+if(result != 0)  //!= ei võrdu
+{
+    Console.WriteLine("User number is odd.");
+}
+else
+{
+    Console.WriteLine("User number is even");
+}
---
+++ Week3IfAndSwitch/Program.cs
@@ -0,0 +1,9 @@
+﻿//modulus - %  jäägi otsimine
+
+int a = 24;
+int b = 8;
+
+int c = a % b; // + - * /
+//int ümmardab
+
+Console.WriteLine(c);
